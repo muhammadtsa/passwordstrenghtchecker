880cabb94355ab95766ce386921b7cc79042e781
unknown <muhammadtsamir05@gmail.com>
2024-05-08 08:25:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+function Strenght(password) {
+    let i = 0;
+    if (password.lenght > 6) {
+        i++;
+    }
+     if (password.lenght >= 10) {
+         i++;
+     }
+
+    if (/[A-Z]/.test(password)) {
+         i++;
+     }
+
+    if (/[0-9]/.test(password)) {
+         i++;
+     }
+
+    if (/[A-Za-z0-8]/.test(password)) {
+         i++;
+     }
+
+return i;
+}
+
+let container =document.querySelector(".container");
+document.addEventListener("keyup", function (e) {
+    let password = document.querySelector("#YourPassword").value;
+
+     let strength = Strength(password);
+     if (strength <= 2) {
+       container.classList.add("weak");
+       container.classList.remove("moderate");
+       container.classList.remove("strong");
+     } else if (strenght >= 2 && strenght <= 4) {
+       container.classList.remove("weak");
+       container.classList.add("moderate");
+       container.classList.remove("strong");
+      } else {
+       container.classList.remove("weak");
+       container.classList.remove("moderate");
+       container.classList.add("strong");
+     }
+   });
+
+   let password = document.querySelector("#YourPassword");
+   let show = document.querySelector(".show");
+   show.onclick = function () {
+    if  (password.type === "password") {
+        password.setAttribute("type", "text");
+        show.classList.add("hide");
+
+    } else {
+        password.setAttribute("type", "password");
+        show.classList.remove("hide");
+    }
+};
+
+
